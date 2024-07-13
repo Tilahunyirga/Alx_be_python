951f17db9bf9d60037caaa06cc1b8bdfe527fdf3
tilahun <tilahunyirga48@gmail.com>
2024-07-13 20:12:31 +0300

    library management

---
+++ programming_paradigm/library_management.py
@@ -0,0 +1,31 @@
+class Book:
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+        self._is_checked_out = False
+class Library :
+    def __init__(self):
+        self._books=[]
+    def add_book(self,book:Book):
+        self._books.append(book)
+
+
+    def check_out_book(self, title):
+        for book in self._books:
+            if book.title==title and book._is_checked_out==False:
+                book._is_checked_out=True
+
+                return book
+
+    def return_book(self, title):
+        for book in self._books:
+            if book.title == title and book._is_checked_out == True:
+                book._is_checked_out = False
+                #
+                return book
+
+    def list_available_books(self):
+        for book in self._books:
+            if book._is_checked_out==False:
+                print(f"{book.title} {book.author}")
+
