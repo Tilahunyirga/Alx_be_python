a0e342f580e8e6c5aebd37b425d7cb06c57d0322
tilahun <tilahunyirga48@gmail.com>
2024-07-19 15:26:00 +0300

    fix chars

--- oop/book_class.py
+++ oop/book_class.py
@@ -4,7 +4,7 @@ class Book:
         self.title, self.author, self.year = title, author, year
 
     def __del__(self):
-        print(f"Deleting {self.title} ")
+        print(f"Deleting {self.title}")
 
     def __str__(self):
         return f"{self.title} by {self.author}, published in {self.year}"
