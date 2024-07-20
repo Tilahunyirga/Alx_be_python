da83d44689c23ca8a7269bc674ff11878bd817c6
tilahun <tilahunyirga48@gmail.com>
2024-07-20 11:30:56 +0300

    fix self.books

--- oop/library_system.py
+++ oop/library_system.py
@@ -22,7 +22,7 @@ class PrintBook(Book):
 
 class Library:
     def __init__(self):
-        self.books =[]
+        self.books = []
 
     def add_book(self, book):
         if isinstance(book, Book):
