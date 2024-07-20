6f124e31fbc83a68fd933d34e1dee417e8782149
tilahun <tilahunyirga48@gmail.com>
2024-07-20 22:54:16 +0300

    my first polymorphism

--- oop/polymorphism_demo.py
+++ oop/polymorphism_demo.py
@@ -11,6 +11,7 @@ class Rectangle(Shape):
     def area(self):
         return ("%.2d" %float(self.width * self.length))
 
+
 class Circle(Shape):
     def __init__(self, radius):
         self.radius = radius
